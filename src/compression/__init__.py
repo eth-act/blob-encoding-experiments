9dbc2c3a77921409459ea2e28274c8ae5bac9073
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-02-01 22:29:38 +0000

    zstd fast and mid

--- src/compression/__init__.py
+++ src/compression/__init__.py
@@ -5,10 +5,31 @@ from .none import NoCompression
 from .zstd import ZstdCompressor
 from .gzip import GzipCompressor
 
+
+class ZstdFastCompressor(ZstdCompressor):
+    """Zstandard at level 3 - fast compression."""
+
+    name = "zstd_fast"
+
+    def __init__(self):
+        super().__init__(level=3)
+
+
+class ZstdMidCompressor(ZstdCompressor):
+    """Zstandard at level 6 - balanced compression."""
+
+    name = "zstd_mid"
+
+    def __init__(self):
+        super().__init__(level=6)
+
+
 # Registry of available compressors
 COMPRESSORS: dict[str, type[Compressor]] = {
     "none": NoCompression,
     "zstd": ZstdCompressor,
+    "zstd_fast": ZstdFastCompressor,
+    "zstd_mid": ZstdMidCompressor,
     "gzip": GzipCompressor,
 }
 
